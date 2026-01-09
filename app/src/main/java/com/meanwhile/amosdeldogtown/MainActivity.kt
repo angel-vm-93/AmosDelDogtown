f5fbe75c73303052d5c3f7f1dd19a0c718b77689
mengujua <juan.mengual@adidas.com>
2026-01-09 12:19:40 +0000

    Initial commit

---
+++ app/src/main/java/com/meanwhile/amosdeldogtown/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.meanwhile.amosdeldogtown
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.meanwhile.amosdeldogtown.ui.theme.AmosDelDogtownTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AmosDelDogtownTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(
+                        modifier = Modifier.padding(innerPadding),
+                        text = "Welcome to ///",
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AmosDelDogtownTheme {
+        Greeting("Android")
+    }
+}
